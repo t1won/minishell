725209bda7e95da5fe26cd09d91f8a050f938a25
Jaeeun Lee <jaeelee@c4r5s6.42seoul.kr>
2021-03-02 12:12:47 +0900

    test

--- incs/minishell.h
+++ incs/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tseo <tseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 10:10:14 by tseo              #+#    #+#             */
-/*   Updated: 2021/03/02 12:03:46 by tseo             ###   ########.fr       */
+/*   Updated: 2021/03/02 12:12:33 by jaeelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,5 @@
 #include "../libft/libft.h"
 #include <stdlib.h>
 
+# define TEST 10;
 #endif
