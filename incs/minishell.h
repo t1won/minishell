be07b78f204e29deae3710f9a2878f842e48f876
Jaeeun Lee <jaeelee@c4r5s6.42seoul.kr>
2021-03-02 12:06:04 +0900

    test

--- incs/minishell.h
+++ incs/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tseo <tseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 10:10:14 by tseo              #+#    #+#             */
-/*   Updated: 2021/03/02 10:12:05 by tseo             ###   ########.fr       */
+/*   Updated: 2021/03/02 12:05:55 by jaeelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 # define MINISHELL_H
 #include <stdio.h>
 #include "../libft/libft.h"
+
+# define TEST 10;
 
 #endif
