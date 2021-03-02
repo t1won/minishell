36ee6b09b7635e749e96ff39a99eecd9f7e78a9f
Taewon Seo <tseo@c4r5s8.42seoul.kr>
2021-03-02 12:14:02 +0900

    updated

--- incs/minishell.h
+++ incs/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tseo <tseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 10:10:14 by tseo              #+#    #+#             */
-/*   Updated: 2021/03/02 12:12:33 by jaeelee          ###   ########.fr       */
+/*   Updated: 2021/03/02 12:13:54 by tseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@
 # define MINISHELL_H
 
 #include <stdio.h>
-#include "../libft/libft.h"
 #include <stdlib.h>
+#include "../libft/libft.h"
 
-# define TEST 10;
 #endif
