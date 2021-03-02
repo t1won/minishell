ee1538dcdd7f4704f96818aaf28d54e223e6b22c
Taewon Seo <tseo@c4r5s8.42seoul.kr>
2021-03-02 13:11:34 +0900

    added gnl

--- incs/minishell.h
+++ incs/minishell.h
@@ -6,15 +6,19 @@
 /*   By: tseo <tseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 10:10:14 by tseo              #+#    #+#             */
-/*   Updated: 2021/03/02 12:03:46 by tseo             ###   ########.fr       */
+/*   Updated: 2021/03/02 13:10:06 by tseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdio.h>
-#include "../libft/libft.h"
-#include <stdlib.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+# include "../libft/libft.h"
+
+# define MAX_FD 1024
+# define GNL_BUFFER_SIZE 25
 
 #endif
---
+++ srcs/main_utils/gnl.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gnl.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tseo <tseo@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/02 13:09:06 by tseo              #+#    #+#             */
+/*   Updated: 2021/03/02 13:09:13 by tseo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incs/minishell.h"
+
+char			*ft_strappend(char *s1, char *s2)
+{
+	char *tmp;
+
+	if (!s1)
+	{
+		s1 = ft_strdup(s2);
+		return (s1);
+	}
+	else
+	{
+		tmp = ft_strjoin(s1, s2);
+		free(s1);
+		s1 = tmp;
+		return (s1);
+	}
+}
+
+static int		get_line(char **strs, char **line, char *ptr, int fd)
+{
+	char *tmp;
+
+	if (ptr != 0)
+	{
+		*ptr = 0;
+		*line = ft_strdup(strs[fd]);
+		tmp = ft_strdup(ptr + 1);
+		free(strs[fd]);
+		strs[fd] = tmp;
+		return (1);
+	}
+	if (strs[fd] != 0)
+	{
+		*line = strs[fd];
+		strs[fd] = 0;
+	}
+	else
+		*line = ft_strdup("");
+	return (0);
+}
+
+int				get_next_line(int fd, char **line)
+{
+	static char		*strs[MAX_FD];
+	char			buf[GNL_BUFFER_SIZE + 1];
+	int				rd_size;
+	char			*ptr;
+
+	if (fd < 0 || !line || GNL_BUFFER_SIZE < 1)
+		return (-1);
+	while ((ptr = ft_strchr(strs[fd], '\n')) == 0
+			&& (rd_size = read(fd, buf, GNL_BUFFER_SIZE)) > 0)
+	{
+		buf[rd_size] = 0;
+		strs[fd] = ft_strappend(strs[fd], buf);
+	}
+	if (rd_size < 0)
+		return (-1);
+	return (get_line(strs, line, ptr, fd));
+}
